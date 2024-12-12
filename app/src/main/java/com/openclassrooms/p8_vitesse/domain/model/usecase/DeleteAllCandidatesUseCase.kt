42d4870c864fcd2009b910798db1d5d75d6d7066
Camille Brasselet <xcamille2605x@gmail.com>
2024-12-12 13:09:30 +0100

    ajout de DeleteAllCandidatesUseCase

---
+++ app/src/main/java/com/openclassrooms/p8_vitesse/domain/model/usecase/DeleteAllCandidatesUseCase.kt
@@ -0,0 +1,20 @@
+package com.openclassrooms.p8_vitesse.domain.model.usecase
+
+import com.openclassrooms.p8_vitesse.data.repository.CandidateRepository
+import javax.inject.Inject
+
+/**
+ * Use Case pour supprimer tous les candidats de la base de données.
+ * @param repository Le repository pour gérer les candidats.
+ */
+class DeleteAllCandidatesUseCase @Inject constructor(
+    private val repository: CandidateRepository
+) {
+
+    /**
+     * Exécute la suppression de tous les candidats.
+     */
+    suspend fun execute() {
+        repository.deleteAllCandidates()
+    }
+}
