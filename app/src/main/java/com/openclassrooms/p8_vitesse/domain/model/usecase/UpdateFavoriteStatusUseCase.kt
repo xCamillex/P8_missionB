254c8c6035b1c1210c0630324e719ef038a317c2
Camille Brasselet <xcamille2605x@gmail.com>
2024-12-12 13:07:13 +0100

    ajout de UpdateFavoriteStatusUseCase

---
+++ app/src/main/java/com/openclassrooms/p8_vitesse/domain/model/usecase/UpdateFavoriteStatusUseCase.kt
@@ -0,0 +1,24 @@
+package com.openclassrooms.p8_vitesse.domain.model.usecase
+
+import com.openclassrooms.p8_vitesse.data.repository.CandidateRepository
+import javax.inject.Inject
+
+/**
+ * Use Case pour mettre à jour le statut de favori d'un candidat.
+ *
+ * @property candidateRepository Le repository permettant d'accéder aux données des candidats.
+ */
+class UpdateFavoriteStatusUseCase @Inject constructor(
+    private val candidateRepository: CandidateRepository
+) {
+    /**
+     * Exécute la mise à jour du statut de favori d'un candidat.
+     *
+     * @param id L'identifiant du candidat à mettre à jour.
+     * @param favorite Le nouveau statut de favori.
+     * @return Le nombre de lignes affectées.
+     */
+    suspend fun execute(id: Long, favorite: Boolean): Int {
+        return candidateRepository.updateFavoriteStatus(id, favorite)
+    }
+}
